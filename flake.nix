b8b5144d2274030169cd72a43dd68ae2419b020d
Parker Britt <parker@parkerbritt.com>
2025-04-19 19:49:22 +0100

    fix: fix flake inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
