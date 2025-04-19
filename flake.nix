72191772e7408691a5fddc65b5bf0fa7d70ccf9a
Parker Britt <parker@parkerbritt.com>
2025-04-19 19:45:43 +0100

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "";
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            gcc9
+            SDL2
+            SDL2.dev
+          ];
+        };
+      };
+    };
+}
+
