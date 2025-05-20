7b20e5b76a208aa209d089a3d23eeba9c8e52c68
parker <parker.g.britt@gmail.com>
2025-05-21 00:29:57 +0100

    feat: add simple shading

--- src/shaders/sphereFS.glsl
+++ src/shaders/sphereFS.glsl
@@ -1,7 +1,23 @@
-#version 410 core
+#version 430 core
 
+in vec4 vWorldPos;
+in vec3 vNormal;
 out vec4 color;
+
+float remap(float value, float inMin, float inMax, float outMin, float outMax)
+{
+    return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
+vec3 remap(vec3 value, vec3 inMin, vec3 inMax, vec3 outMin, vec3 outMax)
+{
+    return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
 void main()
 {
-   color = vec4(1.0f, 0.5, 0.0f, 1.0f);
+    // color = vWorldPos;
+    float brightness = remap(dot(vNormal, vec3(1.0,0.0,0.0)), -1, 1, 0.5, 1);
+    color = vec4(vec3(brightness), 1.0f);
+    // color = vec4(1.0f, 0.5, 0.0f, 1.0f);
 }
--- src/shaders/sphereVS.glsl
+++ src/shaders/sphereVS.glsl
@@ -3,16 +3,22 @@
 in vec4 position;
 uniform mat4 uView;
 uniform mat4 uProj;
-uniform vec3 offsets[1000];
+layout(location = 1) in vec3 aNormal;
 
 layout(std430, binding = 3) buffer InstancePositionBuffer {
     vec4 instancePositions[];
 };
 
+out vec4 vWorldPos;
+out vec3 vNormal;
+
 void main()
 {
+    vNormal = aNormal;
     vec4 offset = instancePositions[gl_InstanceID];
+    vWorldPos = uProj * (vec4(position.xyz, position.w)+offset); 
+    vec4 vViewPos = uProj * uView * (vec4(position.xyz, position.w)+offset);
     
-    gl_Position = uProj * uView * (vec4(position.xyz, position.w)+offset);
+    gl_Position = vViewPos;
 }
 
